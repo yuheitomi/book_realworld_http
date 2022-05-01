d86f43e05af2fb45e8fa3df2f24d4d419f8a2f4e
Yuhei Kuratomi <yuhei.kuratomi@gmail.com>
2022-05-02 05:58:58 +0900

    response with header

--- rust/echoserv/src/bin/echoserv_2.rs
+++ rust/echoserv/src/bin/echoserv_2.rs
@@ -1,9 +1,9 @@
 /// Simple echo server using `tiny_http` crate
 use std::io::prelude::*;
-use tiny_http::{Response, Server};
+use tiny_http::{Header, Response, Server};
 
 fn main() {
-    let addr = "0.0.0.0:18888";
+    let addr = "127.0.0.1:18888";
 
     let server = Server::http(addr).expect("Failed to initiate server");
     for req in server.incoming_requests() {
@@ -17,7 +17,14 @@ fn main() {
             println!("{:20} {}", header.field, header.value);
         }
 
-        let res = Response::from_string("Hello");
+        let content_type = "Content-Type: text/html".parse::<Header>().unwrap();
+
+        let res = Response::from_string(format!(
+            "<html><body>Hello. <a href=\"{}\">link</a></body></html>",
+            addr
+        ))
+        .with_status_code(200)
+        .with_header(content_type);
         req.respond(res).unwrap();
     }
 }
